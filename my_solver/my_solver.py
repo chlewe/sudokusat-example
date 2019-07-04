c90ca7edc199b87f30039c6545c36a7885d784b9
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2019-07-05 00:06:30 +0200

    Expanded main()

--- my_solver/my_solver.py
+++ my_solver/my_solver.py
@@ -4,28 +4,32 @@ import sys
 import os
 
 from encoding import minimal_encoding
+from sudoku_io import *
 from sys import stderr
-from sudoku_io import parse_sudoku, parse_solver_output, clauses_to_cnf
 
 if __name__ == "__main__":
-    #if len(sys.argv) < 2:
-    #    print("Usage: python3 my_solver.py [sat_solver] [path_to_task]")
-    #    sys.exit(1)
+    if len(sys.argv) < 2:
+        print("Usage: python3 my_solver.py [sat_solver] [path_to_task]")
+        sys.exit(1)
 
-    #executable = sys.argv[0]
-    #sat_solver = sys.argv[1]
-    #path_to_task = sys.argv[2]
-    #
-    #print("running {} {} {}".format(executable, sat_solver, path_to_task), file=stderr)
-    #
-    #if os.path.basename(path_to_task) == "bsp-sudoku1.txt":
-    #    sol_dir = os.path.dirname(path_to_task)
-    #    sol_file = os.path.basename(path_to_task).replace(".txt", ".sol")
-    #    with open("{}/{}".format(sol_dir, sol_file), "r") as f:
-    #        for line in f:
-    #            print(line)
-    #print("done!", file=stderr)
+    executable = sys.argv[0]
+    sat_solver = sys.argv[1]
+    path_to_task = sys.argv[2]
 
-    variables, constraints = parse_sudoku("/home/user/satSolving/sudoku-sat/examples/bsp-sudoku-input.txt")
+    print("running {} {} {}".format(executable, sat_solver, path_to_task), file=stderr)
+
+    cnf_file = "/home/user/satSolving/sudokusat-example/my_solver/sudoku.cnf"
+
+    print("Generating CNF file...", file=stderr)
+    variables, constraints = parse_sudoku(path_to_task)
     sat_problem = minimal_encoding(variables, constraints)
-    print(clauses_to_cnf(sat_problem, variables))
+    clauses_to_cnf_file(sat_problem, variables, cnf_file)
+
+    print("Waiting for the SAT solver...", file=stderr)
+    output = call_solver(sat_solver, cnf_file)
+    print(output)
+
+    print("Generating solution from solver output...", file=stderr)
+    parse_solver_output(output, variables)
+
+    print("Done.", file=stderr)
