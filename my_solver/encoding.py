cb0955b8beb7ec18a442c9863b2a89a683e5380f
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2019-07-05 00:04:30 +0200

    Disabled encoding for satisfiability
    - encoding needs fixing in the future

--- my_solver/encoding.py
+++ my_solver/encoding.py
@@ -9,35 +9,42 @@ def minimal_encoding(variables, constraints):
 
     """
     bigwedge_{x=1}^{9} bigwedge_{y=1}^{9} bigvee_{z=1}{9} s_xyz
+
+    "Each cell has at least one number"
     """
     for x in range(width):
         for y in range(width):
             clause = list()
             for z in range(1, width + 1):
-                variable = variables.get_index(x, y, z)
-                clause.append(variable)
+                clause.append(variables.get_index(x, y, z))
 
             clauses.append(clause)
 
     """
     bigwedge_{y=1}^{9} bigwedge_{z=1}^{9} bigwedge_{x=1}{8} bigwedge_{i=x+1}^{9} (neg s_xyz or neg x_iyz)
+
+    "Each number occurs at most once per row"
     """
-    for y in range(width):
-        for z in range(width):
-            for x in range(width - 1):
-                for i in range(x, width):
-                    clause = [-variables.get_index(x, y, z), -variables.get_index(i, y, z)]
-                    clauses.append(clause)
+    # FIXME
+    #for y in range(width):
+    #    for z in range(1, width + 1):
+    #        for x in range(width - 1):
+    #            for i in range(x, width):
+    #                clause = [-variables.get_index(x, y, z), -variables.get_index(i, y, z)]
+    #                clauses.append(clause)
 
     """
     bigwedge_{x=1}^{9} bigwedge_{z=1}^{9} bigwedge_{y=1}{8} bigwedge_{i=y+1}^{9} (neg s_xyz or neg x_xiz)
+
+    "Each number occurs at most once per column"
     """
-    for x in range(width):
-        for z in range(width):
-            for y in range(width - 1):
-                for i in range(y, width):
-                    clause = [-variables.get_index(x, y, z), -variables.get_index(x, i, z)]
-                    clauses.append(clause)
+    # FIXME
+    #for x in range(width):
+    #    for z in range(1, width + 1):
+    #        for y in range(width - 1):
+    #            for i in range(y, width):
+    #                clause = [-variables.get_index(x, y, z), -variables.get_index(x, i, z)]
+    #                clauses.append(clause)
 
     """
     bigwedge_{z=1}^{9} bigwedge_{i = 0}^{2} bigwedge_{j = 0}^{2} bigwedge_{x=1}^{3} bigwedge_{y=1}{3} bigwedge_{k=y+1}^{3} (neg s_(3i+x)(3j+y)z or neg x_(3i+x)(3j+k)z)
