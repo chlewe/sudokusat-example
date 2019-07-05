fa001c5a8b7603c3ae4dd48975ba7abbe3b62a8f
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2019-07-05 16:43:43 +0200

    Disposed of old Variables and built them from new

--- my_solver/sudoku.py
+++ my_solver/sudoku.py
@@ -1,60 +1,38 @@
-from math import floor, sqrt
-
-_next_index_counter = 0
-def _next_index():
-    global _next_index_counter
-    _next_index_counter += 1
-    return _next_index_counter
-
-_xyz_index_bijection = set()
-
-def _get_index(x, y, z):
-    for (xyz, index) in _xyz_index_bijection:
-        if (x, y, z) == xyz:
-            return index
-
-    index = _next_index()
-    _xyz_index_bijection.add(((x, y, z), index))
-    return index
-
-def _get_xyz(index):
-    for ((x, y, z), _index) in _xyz_index_bijection:
-        if index == _index:
-            return (x, y, z + 1)
-    return None
-
-def _index_to_coordinates(index):
-    return _get_xyz(index)
-
+from math import floor, sqrt, log10
 
 class Variables:
     def __init__(self, width):
         self.width = width
-        self.grid_width = floor(sqrt(width))
-        self.grids_per_dim = self.width // self.grid_width
-        self._indices = [[[_get_index(x, y, z) for z in range(width)] for y in range(width)] for x in range(width)]
-        self._values = [[[None for z in range(width)] for y in range(width)] for x in range(width)]
+        self.subgrid_width = floor(sqrt(width))
+        self.subgrids_per_dim = self.subgrid_width
+        self.cell_width = floor(log10(width)) + 1
+
+        self._values = [[None for y in range(width)] for x in range(width)]
 
     def get_index(self, row_x, column_y, number_z):
-        return self._indices[row_x][column_y][number_z - 1]
+        return row_x * self.width ** 2 + column_y * self.width + number_z
 
-    def get_value(self, row_x, column_y, number_z):
-        return self._values[row_x][column_y][number_z - 1]
+    def get_value(self, row, column):
+        return self._values[row][column]
 
-    def set_value(self, index, value):
-        row_x, column_y, number_z = _index_to_coordinates(index)
-        self._values[row_x][column_y][number_z - 1] = value
+    def set_value(self, index):
+        row, column, number = self._index_to_coordinates(index)
+        self._values[row][column] = number
 
     def get_max_index(self):
-        return _next_index_counter
+        return self.get_index(self.width - 1, self.width - 1, self.width)
+
+    def _index_to_coordinates(self, index):
+        number = index % self.width
+        number = self.width if number == 0 else number
+        column = (index - number) % (self.width ** 2) // self.width
+        row = (index - column * self.width - number) // self.width ** 2
+        return row, column, number
 
     def __str__(self):
-        variables_str = ""
         for x in range(self.width):
+            line = ""
             for y in range(self.width):
-                variables_str += "({:2},{:2}): {}\n".format(x, y, self._indices[x][y])
-        for x in range(self.width):
-            for y in range(self.width):
-                variables_str += "({:2},{:2}): {}\n".format(x, y, self._values[x][y])
+                line += "{} ".format(self.get_value(x, y))
 
-        return variables_str
+            print(line)
