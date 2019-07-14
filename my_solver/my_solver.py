d31f0001f7b2bc0a3f09bcdd5c2c40e0df0ddfcb
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2019-07-14 17:32:25 +0200

    Updated main
    - incorporated changes of Sudoku and encoding.py
    - CNF file will now be named after sudoku file (was hashed before)

--- my_solver/my_solver.py
+++ my_solver/my_solver.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-import hashlib
 import sys
 import os
 
-from encoding import *
+from encoding import sudoku_to_clauses
 from sudoku_io import *
 from sys import stderr
 
@@ -19,26 +18,25 @@ if __name__ == "__main__":
 
     print("running {} {} {}".format(executable, sat_solver, path_to_task), file=stderr)
 
-
     print("Generating CNF file...", file=stderr)
-    variables, constraints, header = parse_sudoku(path_to_task)
-    sat_problem = extended_encoding(variables, constraints)
+    sudoku, header = parse_sudoku(path_to_task)
+    sat_problem = sudoku_to_clauses(sudoku)
 
-    header_hash = hashlib.md5()
-    header_hash.update(header.encode('UTF-8'))
     cnf_dir = os.path.dirname(__file__)
-    cnf_file = os.path.join(cnf_dir, "cnf-files", header_hash.hexdigest()) + ".cnf"
-
+    cnf_basename = os.path.basename(path_to_task).replace(".txt", ".cnf")
+    cnf_file = os.path.join(cnf_dir, "cnf-files", cnf_basename)
     if not os.path.exists(os.path.dirname(cnf_file)):
         os.makedirs(os.path.dirname(cnf_file))
 
-    clauses_to_cnf_file(sat_problem, variables, cnf_file)
+    print("Saving clauses to \'{}\'...".format(cnf_file), file=stderr)
+    clauses_to_cnf_file(sat_problem, sudoku, cnf_file)
 
     print("Waiting for SAT solver...", file=stderr)
     output = call_solver(sat_solver, cnf_file)
 
     print("Generating solution from solver output...", file=stderr)
-    parse_solver_output(output, variables)
+    parse_solver_output(output, sudoku)
 
-    variables_to_sudoku(variables, header)
+    print(header)
+    print(sudoku)
     print("Done.", file=stderr)
